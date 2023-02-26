2ed169823e34363a6ebffa576abf50809ee90188
Gary Rosa Valencia <asenvhale@gmail.com>
2023-02-26 10:55:32 -0400

    Añadiendo permisos En android para internet y cambiando el los botone por colum a listviw

--- lib/views/buttons_controller.dart
+++ lib/views/buttons_controller.dart
@@ -1,65 +1,66 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 
-class Controller extends StatelessWidget {
+class Controller extends StatefulWidget {
   const Controller({super.key, required this.servidor});
 
   final Socket servidor;
+
+  @override
+  State<Controller> createState() => _ControllerState();
+}
+
+class _ControllerState extends State<Controller> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        IconButton(
-            iconSize: 50,
-            padding: const EdgeInsets.all(40),
-            onPressed: () {
-              servidor.write('up');
-            },
-            icon: const Icon(Icons.arrow_upward)),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          IconButton(
-              iconSize: 50,
-              onPressed: () {
-                servidor.write('left');
-              },
-              padding: const EdgeInsets.all(40),
-              icon: const Icon(Icons.arrow_back_rounded)),
+      body: ListView(
+        children: <Widget>[
           IconButton(
               iconSize: 50,
               padding: const EdgeInsets.all(40),
               onPressed: () {
-                servidor.write('rigth');
+                widget.servidor.write('up');
               },
-              icon: const Icon(Icons.arrow_forward))
-        ]),
-        IconButton(
-            padding: const EdgeInsets.all(40),
-            onPressed: () {
-              servidor.write('down');
-            },
-            iconSize: 50,
-            icon: const Icon(Icons.arrow_downward),
-            style: ElevatedButton.styleFrom(
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero,
-                side: BorderSide(color: Colors.black),
-              ),
-            )),
-        SizedBox(
-          height: 50,
-          child: OutlinedButton(
+              icon: const Icon(Icons.arrow_upward)),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            IconButton(
+                iconSize: 50,
+                onPressed: () {
+                  widget.servidor.write('left');
+                },
+                padding: const EdgeInsets.all(40),
+                icon: const Icon(Icons.arrow_back_rounded)),
+            IconButton(
+                iconSize: 50,
+                padding: const EdgeInsets.all(40),
+                onPressed: () {
+                  widget.servidor.write('rigth');
+                },
+                icon: const Icon(Icons.arrow_forward))
+          ]),
+          IconButton(
+              padding: const EdgeInsets.all(40),
               onPressed: () {
-                servidor.close();
-                Navigator.pushNamed(context, '/');
+                widget.servidor.write('down');
               },
-              child: const Text(
-                "Desconectar",
-                textScaleFactor: 1.5,
+              iconSize: 50,
+              icon: const Icon(Icons.arrow_downward),
+              style: ElevatedButton.styleFrom(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                  side: BorderSide(color: Colors.black),
+                ),
               )),
-        )
-      ],
-    ));
+          ElevatedButton(
+            onPressed: () {
+              widget.servidor.close();
+              Navigator.pushNamed(context, '/');
+            },
+            child: const Text('Desconectar'),
+          ),
+        ],
+      ),
+    );
   }
 }
